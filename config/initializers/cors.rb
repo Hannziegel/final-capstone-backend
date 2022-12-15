90984ddb519c63a4eb30c8bc0a096b201b0608af
karlavdelgadof <karlavdelgadof@gmail.com>
2022-12-14 20:56:19 -0500

    Expose authorization to receive tokens

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -12,5 +12,6 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     resource "*",
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      expose: [:Authorization]
   end
 end

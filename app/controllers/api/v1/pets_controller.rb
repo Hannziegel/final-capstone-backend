2666237441b46f9d468128f9e8582529a45cfc30
Rodrigo Johann Reckziegel Nunes <hannziegel@gmail.com>
2022-12-21 22:26:08 -0300

    Add pets controller and routes

---
+++ app/controllers/api/v1/pets_controller.rb
@@ -0,0 +1,51 @@
+class Api::V1::PetsController < ApplicationController
+  before_action :set_pet, only: %i[ show update destroy ]
+
+  # GET /pets
+  def index
+    @pets = pet.all
+
+    render json: @pets
+  end
+
+  # GET /pets/1
+  def show
+    render json: @pet
+  end
+
+  # POST /pets
+  def create
+    @pet = pet.new(pet_params)
+
+    if @pet.save
+      render json: @pet, status: :created
+    else
+      render json: @pet.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /pets/1
+  def update
+    if @pet.update(pet_params)
+      render json: @pet
+    else
+      render json: @pet.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /pets/1
+  def destroy
+    @pet.destroy
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_pet
+      @pet = Pet.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def pet_params
+      params.require(:pet).permit(:accommodation_id, :name, :size, :information, :specie)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :accommodations
       resources :reservations
+      resources :pets
     end
   end
 end
